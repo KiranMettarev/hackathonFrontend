d8b9f4e9c8e9b3501c05ef640920d28a185ce750
Kiran Bhole <kiran.bhole@mettarev.com>
2024-10-24 22:31:41 +0530

    solve error.

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -11,7 +11,7 @@ import ApexCharts from "apexcharts";
 })
 export class HomeComponent {
 
-  step: number = 4;
+  step: number = 1;
   showUploadType: boolean = false;
   aadharFlag: boolean = false
   panFlag: boolean = false
@@ -19,12 +19,6 @@ export class HomeComponent {
 
   addAccount(): void {
     this.showUploadType = true;
-  }
-
-  ngOnInit(){
-     setTimeout(() => {
-       this.initChartData()
-   }, 1000);
   }
 
   fetchKYC(iVal: string): void {
@@ -116,102 +110,5 @@ export class HomeComponent {
   //   this.panDataFlag = true
   //   console.log(pan, "workPan");
   // }
-
-   options:any = {};
-   chart: any;
-
-   initChartData(): void {
-    console.error('Chart Load Call')
-    this.options = {
-      series: [{
-        name: 'Total Debit',
-        data: ['81578.5', '81862.14', '705768.97', '279885.39','78501.18', '144600.03', '0']
-      },{
-          name: 'Total Credit',
-          data: ['81703.25', '130483', '949453', '1819','65811', '228708', '306']
-      },{
-        name: 'Average Monthly Balances',
-        data: ['604.57', '49225.43', '292909.46', '14843.07','2152.89', '86260.86', '86566.86']
-    }],
-      chart: {
-      height: '400px',
-      width: '350px',
-      // type: 'line',
-      type: 'area',
-      zoom: {
-        enabled: false
-      },
-      toolbar: {
-        show: true,
-        tools: {
-          download: false,
-          selection:false,
-          pan:false,
-          reset:false,
-          zoom:false,
-        }
-      }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      
-      stroke: {
-        //  curve: 'straight',
-        curve:'smooth',
-        show:true,
-        width: 2,
-      },
-      markers: {
-        size: 4,
-        colors: '#fff',
-        strokeColors: ['#008ffb','#00e396','#feb019'],
-        strokeWidth: 2,
-        strokeOpacity: 0.9,
-        strokeDashArray: 0,
-        fillOpacity: 1,
-        discrete: [],
-        shape: "circle",
-        offsetX: 0,
-        offsetY: 0,
-        onClick: undefined,
-        onDblClick: undefined,
-        showNullDataPoints: true,
-        hover: {
-          size: undefined,
-          sizeOffset: 2
-        }
-    },
-      title: {
-        text: '',
-        align: 'left'
-      },
-      grid: {
-        strokeDashArray: 4,
-        xaxis: {
-            lines: {
-                show: false
-            }
-        },
-        row: {
-          opacity: 0.5
-        },
-      },
-      legend: { show: true,position:'top',horizontalAlign:'right' },
-      xaxis: {
-        labels: {
-          rotate: 0
-        },
-        categories: ["Oct 2022", "Nov 2022", "Dec 2022", "Jan 2023","Feb 2023", "Mar 2023", "Apr 2023"],  
-      },
-      tooltip: {
-        // y: {
-        //   formatter: (value:any) => { if(value != null || value != undefined){return value + '$' }}
-        // },
-      }
-    };
-        this.chart = new ApexCharts(document.querySelector("#chartnew"), this.options);
-        this.chart.render();
-  }
 
 }
